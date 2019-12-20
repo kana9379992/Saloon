a5cdf642ee5291c48e7c02c7db490f5bde342191
Kanagat Amanbaev <amanbaev.kanagat@gmail.com>
2019-12-20 20:45:57 +0600

    BG change, tried parallax

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-   })
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)'
+});
