cc15b5e762cca42ce2500f43c02259ffa4bf014a
Kanagat Amanbaev <amanbaev.kanagat@gmail.com>
2020-02-06 19:53:41 +0600

    slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-  
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '10%',
+        slidesToShow: 5,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+});
