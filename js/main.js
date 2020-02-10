2162040c52f792ddf598e5f73f8a7d305ad1b0da
Kanagat Amanbaev <amanbaev.kanagat@gmail.com>
2020-02-10 20:35:19 +0600

    review block

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        centerMode: true,
-        centerPadding: '10%',
-        slidesToShow: 5,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
-});
+  $('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+})
